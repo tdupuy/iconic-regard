105ebc38b82820a258f0d197966e7a5eaa7b068a
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-03-28 11:58:47 +0100

    refactor: :building_construction: transition to atomic design home menu

--- src/lib/client/components/atoms/BrandName/brand-name.svelte
+++ src/lib/client/components/atoms/BrandName/brand-name.svelte
@@ -1,5 +1,1 @@
-<h1
-	class="anim-2 text-base-content mb-[clamp(0.25rem,0.8vw,0.4rem)] text-[clamp(2.4rem,8vw,5.2rem)] leading-none"
->
-	Iconic Regard
-</h1>
+<h1 class="text-base-content mb-2 text-5xl">Iconic Regard</h1>
