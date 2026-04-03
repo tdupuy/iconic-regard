7016bfccf3935dcbd018e55b83e47c46b4000c56
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-04-03 17:49:38 +0200

    feat: :sparkles: add a booking route and page

--- src/lib/client/components/organisms/Navbar/navbar.svelte
+++ src/lib/client/components/organisms/Navbar/navbar.svelte
@@ -7,11 +7,12 @@
 <div class="navbar bg-base-100 shadow-sm">
 	<div class="flex-1"></div>
 	<div class="mr-2 flex flex-none items-center gap-3">
-		<button
+		<a
+			href="/booking"
 			class="btn text-base-100 shadow-secondary/20 from-secondary to-primary rounded-full border-0 bg-gradient-to-r px-6 py-2 shadow-lg transition-transform hover:scale-[1.02]"
 		>
 			Prendre rendez-vous
-		</button>
+		</a>
 
 		<button
 			type="button"
---
+++ src/routes/(app)/booking/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Cal from '$lib/client/components/organisms/Cal';
+	import { PUBLIC_CAL_LINK } from '$env/static/public';
+</script>
+
+<!--<Cal embedType="inline" namespace="mes-prestations" calLink="{PUBLIC_CAL_LINK}/30min" />-->
+<Cal namespace="15min" />
+<button
+	data-cal-link="{PUBLIC_CAL_LINK}/30min"
+	data-cal-namespace="15min"
+	class="rounded-lg bg-blue-600 px-6 py-3 text-white shadow-md transition-colors hover:bg-blue-700"
+>
+	Book a 15min Meeting
+</button>
