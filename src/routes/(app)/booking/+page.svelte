aa3883b2e1f168a240ae8b8467a5e45b421d9268
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-04-08 21:27:26 +0200

    fix: :lipstick: add a responsive container

--- src/routes/(app)/booking/+page.svelte
+++ src/routes/(app)/booking/+page.svelte
@@ -2,11 +2,27 @@
 	import ServiceItem from '$lib/client/components/organisms/ServiceItem';
 </script>
 
-<ServiceItem
-	namespace="15min"
-	imgUrl="src/lib/client/assets/lash-lift-corean.webp"
-	category="Soin des cils"
-	service="Lash Lift Coréen"
-	duration="15 min"
-	price="25 €"
-/>
+<!-- Loop Between both components -->
+<div class="md:hidden">
+	<ServiceItem
+		namespace="15min"
+		imgUrl="src/lib/client/assets/lash-lift-corean.webp"
+		category="Soin des cils"
+		service="Lash Lift Coréen"
+		duration="15 min"
+		price="25 €"
+	/>
+</div>
+
+<div class="hidden min-h-screen bg-purple-50/10 py-16 md:block">
+	<div class="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
+		<ServiceItem
+			namespace="15min"
+			imgUrl="src/lib/client/assets/lash-lift-corean.webp"
+			category="Soin des cils"
+			service="Lash Lift Coréen"
+			duration="15 min"
+			price="25 €"
+		/>
+	</div>
+</div>
