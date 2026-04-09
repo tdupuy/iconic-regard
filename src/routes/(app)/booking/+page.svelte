eced9fe8e8d6164a016181d75c8b96f0b7bd4323
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-04-09 21:02:17 +0200

    refactor(drizzle): :card_file_box: add data from db to component

--- src/routes/(app)/booking/+page.svelte
+++ src/routes/(app)/booking/+page.svelte
@@ -5,26 +5,28 @@
 </script>
 
 <!-- Loop Between both components -->
-<div class="md:hidden">
-	<ServiceItem
-		namespace="15min"
-		imgUrl="src/lib/client/assets/lash-lift-corean.webp"
-		category="Soin des cils"
-		service="Lash Lift Coréen"
-		duration="15 min"
-		price="25 €"
-	/>
-</div>
-
-<div class="hidden min-h-screen bg-purple-50/10 py-16 md:block">
-	<div class="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
+{#each data.services as service (service.id)}
+	<div class="md:hidden">
 		<ServiceItem
-			namespace="15min"
+			namespace={service.slug}
 			imgUrl="src/lib/client/assets/lash-lift-corean.webp"
-			category="Soin des cils"
-			service="Lash Lift Coréen"
-			duration="15 min"
-			price="25 €"
+			category={service.category}
+			service={service.name}
+			duration={service.duration + ' min'}
+			price={service.price + ' €'}
 		/>
 	</div>
-</div>
+
+	<div class="hidden min-h-screen bg-purple-50/10 py-16 md:block">
+		<div class="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
+			<ServiceItem
+				namespace={service.slug}
+				imgUrl="src/lib/client/assets/lash-lift-corean.webp"
+				category={service.category}
+				service={service.name}
+				duration={service.duration + ' min'}
+				price={service.price + ' €'}
+			/>
+		</div>
+	</div>
+{/each}
