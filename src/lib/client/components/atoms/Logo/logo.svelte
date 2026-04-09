01063b702d8f2dfbb138c803f3c8031fe44d39cd
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-04-09 21:30:16 +0200

    fix: logo moves to statis/assets

--- src/lib/client/components/atoms/Logo/logo.svelte
+++ src/lib/client/components/atoms/Logo/logo.svelte
@@ -1,9 +1,5 @@
-<script lang="ts">
-	import logoUrl from '$lib/client/assets/logo.png?url';
-</script>
-
 <img
-	src={logoUrl}
+	src="assets/logo.png"
 	alt="Iconic Regard"
 	class="animate-float mb-3 h-[clamp(120px,30vw,220px)] w-[clamp(120px,30vw,220px)] rounded-full object-contain drop-shadow-lg"
 />
