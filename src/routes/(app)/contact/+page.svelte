99e875d54867f1bccf680c8f580134832e760acf
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-04-12 14:08:39 +0200

    feat: add google maps

--- src/routes/(app)/contact/+page.svelte
+++ src/routes/(app)/contact/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { PageTitle } from '$lib/client/components/atoms/PageTitle';
-	import { Phone, AtSign } from 'lucide-svelte';
+	import { Phone, AtSign, MapPinHouse } from 'lucide-svelte';
 	import { SiInstagram, SiFacebook } from '@icons-pack/svelte-simple-icons';
 	let { data } = $props();
 </script>
@@ -46,3 +46,24 @@
 </div>
 
 <PageTitle title="Où me trouver" />
+
+<div class="mt-5 flex flex-col items-center justify-center">
+	<p
+		class="text-base-content mt-2 mb-5 flex flex-row items-center gap-4 text-center text-[clamp(1rem,1vw,1.5rem)]"
+	>
+		<MapPinHouse size={28} strokeWidth={1.5} class="mb-4" /> 6 rue des grandes carrières<br />75018
+		Paris
+	</p>
+
+	<div class="w-full overflow-hidden rounded-xl border border-gray-200 shadow-sm lg:w-[30%]">
+		<iframe
+			src="https://maps.google.com/maps?q=10+Rue+de+la+Paix+Paris&output=embed"
+			width="100%"
+			height="256"
+			style="border:0"
+			allowfullscreen
+			loading="lazy"
+			title="Localisation de Iconic Regard"
+		></iframe>
+	</div>
+</div>
