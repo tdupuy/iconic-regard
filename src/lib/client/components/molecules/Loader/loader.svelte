e0a7c2205754b86e7d1aeccd1baffc983f817103
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-05-27 21:49:24 +0200

    style: reindent

--- src/lib/client/components/molecules/Loader/loader.svelte
+++ src/lib/client/components/molecules/Loader/loader.svelte
@@ -6,22 +6,38 @@
 	<Logo />
 	<div class="relative h-14 w-14">
 		<div class="absolute inset-0 origin-center animate-spin" style="animation-duration: 1.8s;">
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.4rem; height: 0.4rem; transform: translate(-50%, -50%) rotate(0deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.36rem; height: 0.36rem; transform: translate(-50%, -50%) rotate(45deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.32rem; height: 0.32rem; transform: translate(-50%, -50%) rotate(90deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.28rem; height: 0.28rem; transform: translate(-50%, -50%) rotate(135deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.24rem; height: 0.24rem; transform: translate(-50%, -50%) rotate(180deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.2rem; height: 0.2rem; transform: translate(-50%, -50%) rotate(225deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.16rem; height: 0.16rem; transform: translate(-50%, -50%) rotate(270deg) translateY(-18px);"></span>
-			<span class="absolute left-1/2 top-1/2 rounded-full bg-primary"
-				style="width: 0.12rem; height: 0.12rem; transform: translate(-50%, -50%) rotate(315deg) translateY(-18px);"></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.4rem; height: 0.4rem; transform: translate(-50%, -50%) rotate(0deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.36rem; height: 0.36rem; transform: translate(-50%, -50%) rotate(45deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.32rem; height: 0.32rem; transform: translate(-50%, -50%) rotate(90deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.28rem; height: 0.28rem; transform: translate(-50%, -50%) rotate(135deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.24rem; height: 0.24rem; transform: translate(-50%, -50%) rotate(180deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.2rem; height: 0.2rem; transform: translate(-50%, -50%) rotate(225deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.16rem; height: 0.16rem; transform: translate(-50%, -50%) rotate(270deg) translateY(-18px);"
+			></span>
+			<span
+				class="bg-primary absolute top-1/2 left-1/2 rounded-full"
+				style="width: 0.12rem; height: 0.12rem; transform: translate(-50%, -50%) rotate(315deg) translateY(-18px);"
+			></span>
 		</div>
 	</div>
 </div>
