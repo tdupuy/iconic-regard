0fbc3fb4d596220b324f7694352f13eea1baa7e9
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-06-30 21:26:30 +0200

    feat: create customer table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,15 @@
-import { pgTable, serial, varchar, text, integer } from 'drizzle-orm/pg-core';
+import {
+	pgTable,
+	serial,
+	varchar,
+	text,
+	integer,
+	uuid,
+	timestamp,
+	pgEnum
+} from 'drizzle-orm/pg-core';
+
+export const customerStatusEnum = pgEnum('customer_status', ['pending', 'active', 'cancelled']);
 
 export const services = pgTable('services', {
 	id: serial('id').primaryKey(),
@@ -15,4 +26,14 @@ export const categories = pgTable('categories', {
 	id: serial('id').primaryKey(),
 	name: varchar('name', { length: 100 }).notNull().unique(),
 	slug: varchar('slug', { length: 100 }).notNull().unique()
+});
+
+export const customers = pgTable('customers', {
+	id: uuid('id').defaultRandom().primaryKey(),
+	name: varchar('name', { length: 100 }).notNull(),
+	email: varchar('email', { length: 255 }).unique(),
+	phoneNumber: varchar('phone_number', { length: 20 }).notNull().unique(),
+	status: customerStatusEnum('status').default('pending').notNull(),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').defaultNow().notNull()
 });
