50857c1525395399edf03de79249c164ff384c17
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-07-02 19:46:07 +0200

    fix: add a "if" for no user to add

--- src/routes/admin/customers/+page.svelte
+++ src/routes/admin/customers/+page.svelte
@@ -6,24 +6,25 @@
 
 <div class="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
 	<h1 class="mb-12 text-4xl font-bold text-slate-900">Clients</h1>
+	{#if data.bookings.length}
+		<section class="rounded-box border-warning/40 bg-warning/5 mb-12 border-2 p-6">
+			<div class="mb-6 flex items-center gap-2">
+				<h2 class="text-xl font-semibold text-slate-900">Nouveaux clients à valider</h2>
+				<span class="badge badge-warning">{data.bookings.length}</span>
+			</div>
 
-	<section class="rounded-box border-warning/40 bg-warning/5 mb-12 border-2 p-6">
-		<div class="mb-6 flex items-center gap-2">
-			<h2 class="text-xl font-semibold text-slate-900">Nouveaux clients à valider</h2>
-			<span class="badge badge-warning">{data.bookings.length}</span>
-		</div>
-
-		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-			{#each data.bookings as booking (booking.id)}
-				<NewCustomerCard
-					email={booking.attendees[0]?.email}
-					phoneNumber={booking.attendees[0]?.phoneNumber}
-					name={booking.attendees[0]?.name}
-					reason={booking.reason}
-				/>
-			{/each}
-		</div>
-	</section>
+			<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+				{#each data.bookings as booking (booking.id)}
+					<NewCustomerCard
+						email={booking.attendees[0]?.email}
+						phoneNumber={booking.attendees[0]?.phoneNumber}
+						name={booking.attendees[0]?.name}
+						reason={booking.reason}
+					/>
+				{/each}
+			</div>
+		</section>
+	{/if}
 
 	<section>
 		<h2 class="mb-6 text-xl font-semibold text-slate-900">Liste des clients</h2>
