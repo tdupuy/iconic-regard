56b4a65e4884466dc05494a6c467b0af7ecebd0a
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-07-24 19:17:03 +0200

    fix: update contact-page

--- src/routes/(app)/contact/+page.svelte
+++ src/routes/(app)/contact/+page.svelte
@@ -9,14 +9,14 @@
 
 <div class="mt-5 mb-10 flex flex-col items-center gap-4">
 	<a
-		href="tel:+33623306352"
+		href="tel:+33766128958"
 		class="text-base-content mt-2 flex flex-row items-center gap-4 text-center text-[clamp(1rem,1vw,1.5rem)]"
 	>
 		<Phone size={28} strokeWidth={1.5} />
-		06 23 30 63 52
+		07 66 12 89 58
 	</a>
 	<a
-		href="mailto:iconic-regard@gmail.com"
+		href="mailto:iconic.regard@gmail.com"
 		class="text-base-content mt-2 flex flex-row items-center gap-4 text-center text-[clamp(1rem,1vw,1.5rem)]"
 	>
 		<AtSign size={28} strokeWidth={1.5} />
@@ -51,13 +51,13 @@
 	<p
 		class="text-base-content mt-2 mb-5 flex flex-row items-center gap-4 text-center text-[clamp(1rem,1vw,1.5rem)]"
 	>
-		<MapPinHouse size={28} strokeWidth={1.5} class="mb-4" /> 6 rue des grandes carrières<br />75018
-		Paris
+		<MapPinHouse size={28} strokeWidth={1.5} class="mb-4" /> 1 Rue nationale<br />72550
+		Coulans-sur-Gée
 	</p>
 
-	<div class="w-full overflow-hidden rounded-xl border border-gray-200 shadow-sm lg:w-[30%]">
+	<div class="mt-10 w-full overflow-hidden rounded-xl border border-gray-200 shadow-sm lg:w-[30%]">
 		<iframe
-			src="https://maps.google.com/maps?q=10+Rue+de+la+Paix+Paris&output=embed"
+			src="https://maps.google.com/maps?q=48.021202528652886,0.013571920140973777&output=embed"
 			width="100%"
 			height="256"
 			style="border:0"
