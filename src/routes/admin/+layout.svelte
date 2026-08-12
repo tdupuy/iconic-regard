00a42d0f89511b528ff1d2651a49bcabc3ae7234
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-08-12 19:46:44 +0200

    fix: enhance admin drawer design

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -17,14 +17,13 @@
 	<title>Admin - Iconic Regard</title>
 </svelte:head>
 
-<app data-theme="violet-perso"
-	><div class="drawer lg:drawer-open">
+<app data-theme="violet-perso">
+	<div class="drawer lg:drawer-open">
 		<input id="my-drawer-4" type="checkbox" class="drawer-toggle" bind:checked={drawerOpen} />
 		<div class="drawer-content">
 			<!-- Navbar -->
-			<nav class="navbar w-full bg-black text-white">
+			<nav class="navbar bg-neutral text-neutral-content min-h-14 w-full">
 				<label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
-					<!-- Sidebar toggle icon -->
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
@@ -33,13 +32,13 @@
 						stroke-width="2"
 						fill="none"
 						stroke="currentColor"
-						class="my-1.5 inline-block size-6"
+						class="text-primary my-1.5 inline-block size-6"
 						><path
 							d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
 						></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg
 					>
 				</label>
-				<div class="px-4 text-lg text-white">Admin iconic-regard</div>
+				<div class="px-4 text-sm font-medium">Admin iconic-regard</div>
 			</nav>
 			<!-- Page content here -->
 			<div class="p-4">
