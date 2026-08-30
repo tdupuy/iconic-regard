e81e9ce750fe116eaffc92c157e728f5e9a9be2a
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-08-30 18:29:28 +0200

    fix: purge useless code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,24 +4,6 @@
 	import '../lib/client/styles/app.css';
 
 	let { children } = $props();
-
-	/*const GA_ID = 'G-F121F18W2B';
-
-	onMount(() => {
-		if (!import.meta.env.PROD) return;
-
-		const script = document.createElement('script');
-		script.async = true;
-		script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-		document.head.appendChild(script);
-
-		window.dataLayer = window.dataLayer || [];
-		function gtag(...args: unknown[]) {
-			window.dataLayer.push(args);
-		}
-		gtag('js', new Date());
-		gtag('config', GA_ID);
-	});*/
 </script>
 
 <svelte:head>
