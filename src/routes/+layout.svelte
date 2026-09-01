2631f6b39e3781e411831714804171c671d273b3
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-09-01 18:42:29 +0200

    fix: add canonical page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import '../lib/client/styles/app.css';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" type="image/png" href="assets/favicon.png" />
+	<link rel="canonical" href={`https://iconic-regard.com${page.url.pathname}`} />
 </svelte:head>
 
 {@render children()}
