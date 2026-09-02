ecb6582010f4f73d8284b4f8826ddfe4336d7ef1
Thibaut dupuy <dupuythibaut@yahoo.fr>
2026-09-02 13:14:43 +0200

    feat: add meta description for home page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,4 +2,10 @@
 	import MainHome from '$lib/client/components/organisms/MainHome';
 </script>
 
+<svelte:head>
+	<meta
+		name="description"
+		content="Iconic Regard, esthéticienne à Coulans (Coulans-sur-Gée). Institut spécialisé dans la beauté du regard : extensions de cils, lash lift, soins sourcils. RDV en ligne."
+	/>
+</svelte:head>
 <MainHome />
