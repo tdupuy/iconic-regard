ed6bf81ad3b1e5960fdf2163f4eac363dfcf74f0
Thibaut Dupuy <dupuythibaut@yahoo.fr>
2026-09-19 12:15:10 +0200

    fix: reverse order of cta buttons in booking

--- src/lib/client/components/organisms/ServiceItem/service-item.svelte
+++ src/lib/client/components/organisms/ServiceItem/service-item.svelte
@@ -97,12 +97,6 @@
 	<div class="mt-2 flex justify-center gap-2">
 		{#if !isService}
 			<button
-				class="rounded border {border} px-4 py-2 {iconImg} hover:{bgTablets}"
-				onclick={() => (expanded = !expanded)}
-			>
-				{expanded ? 'Voir moins' : 'En savoir plus'}
-			</button>
-			<button
 				class="rounded {bgActionBtn} px-4 py-2 font-medium text-white hover:bg-purple-400"
 				data-cal-link={PUBLIC_CAL_LINK + '/' + namespace}
 				data-cal-namespace={namespace}
@@ -110,6 +104,12 @@
 				data-umami-event-service={namespace}
 			>
 				Prendre rendez-vous
+			</button>
+			<button
+				class="rounded border {border} px-4 py-2 {iconImg} hover:{bgTablets}"
+				onclick={() => (expanded = !expanded)}
+			>
+				{expanded ? 'Voir moins' : 'En savoir plus'}
 			</button>
 		{/if}
 	</div>
